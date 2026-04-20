2ef6b2469fdd1c3c0c3b80487115d1580732e39d
Musaleee <aryanmusale87@gmail.com>
2026-04-20 18:28:20 +0530

    Phase 2: Including all files needed into tree.c

--- tree.c
+++ tree.c
@@ -15,6 +15,8 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include "index.h"
+#include "pes.h"
 
 // ─── Mode Constants ─────────────────────────────────────────────────────────
 
