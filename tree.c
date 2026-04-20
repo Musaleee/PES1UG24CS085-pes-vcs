57097fca8c15031d3070950f848286ca898d93a2
Musaleee <aryanmusale87@gmail.com>
2026-04-20 18:29:08 +0530

    Phase 2: Setting up the tree

--- tree.c
+++ tree.c
@@ -132,8 +132,11 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 //
 // Returns 0 on success, -1 on error.
 int tree_from_index(ObjectID *id_out) {
-    // TODO: Implement recursive tree building
-    // (See Lab Appendix for logical steps)
+    Tree tree;
+    tree.count = 0;
+
+    void *data = NULL;
+    size_t len = 0;
     (void)id_out;
     return -1;
 }
